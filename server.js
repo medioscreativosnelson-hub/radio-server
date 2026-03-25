c840c137b0797637c3158e6ace87fcb8a203fe23
medioscreativosnelson-hub <medios.creativosnelson@gmail.com>
2026-03-24 20:05:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,23 +6,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PASSWORD = "1234";
-
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  socket.on("auth", (pass) => {
-    if (pass === PASSWORD) {
-      socket.join("sala");
-      socket.emit("auth_ok");
-    } else {
-      socket.emit("auth_error");
-    }
-  });
 
-  socket.on("offer", (data) => socket.to("sala").emit("offer", data));
-  socket.on("answer", (data) => socket.to("sala").emit("answer", data));
-  socket.on("candidate", (data) => socket.to("sala").emit("candidate", data));
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+  socket.on("candidate", (data) => socket.broadcast.emit("candidate", data));
+
 });
 
-server.listen(3000, () => console.log("Servidor activo"));
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor activo 🔴");
+});
